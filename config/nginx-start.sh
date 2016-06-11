68b79497901d68311d5f26c94e0e7b75d2734c97
Thaweesak Chusri <thaweesak@nimeyo.com>
2016-06-11 12:50:30 +0700

    add clear-cache and dump-autoload

--- config/nginx-start.sh
+++ config/nginx-start.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+
 service nginx start
 tail -f /var/log/nginx/access.log &
 tail -f /var/log/nginx/error.log &
 /usr/sbin/php5-fpm -F
+
+cd /data
+composer self-update
+php artisan cache:clear
+chmod -R 777 storage
+composer dump-autoload
+
+
 while true; do sleep 1d; done
 # nginx -g daemon off;
